7addfdb3e41062ca8665ff47dbe8b7aaf724a962
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2023-12-10 01:26:56 +0700

    First setup

---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Context, loader
+# Create your views here.
+
+def hello (request):
+  return HttpResponse("Hello World")
+
+def index (request):
+  template = loader.get_template('index.html')
+  return HttpResponse(template.render())
---
+++ tubes_pcd_web/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for tubes_pcd_web project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tubes_pcd_web.settings')
+
+application = get_asgi_application()
